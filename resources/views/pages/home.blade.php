c5a28f858707e5d7339ed7af3fbe433d519a1d7c
naykel76 <32632005+naykel76@users.noreply.github.com>
2022-05-05 04:31:41 +1000

    update assets

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,5 @@
-<x-gotime-app-layout layout="{{ config('naykel.template') }}">
+<x-gotime-app-layout layout="{{ config('naykel.template') }}" class="py-5-3-2">
 
+<h1>Home</h1>
 
 </x-gotime-app-layout>
